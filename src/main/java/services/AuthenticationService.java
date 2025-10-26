845c14546bd30e923493e3cd28cfc3d09e0e0bdf
Amr Sabbagh <amr.sabbagh1@gmail.com>
2025-10-26 12:04:12 +0300

    implemented the authentication service class to have authorization end point to be dynamically from system properties or the class with the login method to get the token

--- src/main/java/services/AuthenticationService.java
+++ src/main/java/services/AuthenticationService.java
@@ -1,4 +1,27 @@
 package services;
 
+import client.RestfulBooker;
+import data.Credentials;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.requestSpecification;
+
 public class AuthenticationService {
+
+    private static final String AUTHENTICATION_ENDPOINT = System.getProperty("authorization.endpoint","/auth");
+
+    RestfulBooker client = new RestfulBooker();
+
+    public String getAuthenticationToken(Credentials credentials){
+        return given()
+                    .spec(requestSpecification)
+                .when()
+                    .post(AUTHENTICATION_ENDPOINT)
+                .then()
+                    .extract()
+                    .response()
+                    .jsonPath()
+                    .getString("token");
+    }
+
 }
