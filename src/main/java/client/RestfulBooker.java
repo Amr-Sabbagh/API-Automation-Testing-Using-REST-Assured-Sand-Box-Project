4b50a71046f18ecebb617223e6b9c0557d3123e3
Amr Sabbagh <amr.sabbagh1@gmail.com>
2025-10-26 11:03:06 +0300

    init client class to have the base url and the constructor to build the request specifications

--- src/main/java/client/RestfulBooker.java
+++ src/main/java/client/RestfulBooker.java
@@ -1,5 +1,30 @@
 package client;
 
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
 public class RestfulBooker {
+
+    public RequestSpecification requestSpecification;
+
+    private static final String RESTFUL_BOOKER_BASE_URL = System.getProperty("base.url","https://restful-booker.herokuapp.com");
+
+    public RestfulBooker(){
+        this.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(RESTFUL_BOOKER_BASE_URL)
+                .setAccept(ContentType.JSON)
+                .addHeader("Accept", "application/json")
+                .build();
+    }
+
+    public RestfulBooker(String authToken){
+        this.requestSpecification = new RequestSpecBuilder()
+                .setBaseUri(RESTFUL_BOOKER_BASE_URL)
+                .setAccept(ContentType.JSON)
+                .addHeader("Accept", "application/json")
+                .addCookie(authToken)
+                .build();
+    }
 
 }
